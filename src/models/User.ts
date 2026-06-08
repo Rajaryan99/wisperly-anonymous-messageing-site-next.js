c51b9a9cb10c440d55ef8f975a543afad8af939f
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-08 22:49:06 +0530

    defined userModel with email validation and messageing field

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, {Schema, Document} from "mongoose";
+import { unique } from "next/dist/build/utils";
 
 export interface Message extends Document{
     content: string;
@@ -18,8 +19,64 @@ const MessageSchema: Schema<Message> = new Schema({
         default: Date.now
     },
 
-    name:{
-        type: String,
-    }
     
 })
+
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, 'username is required'],
+        unique: true,
+        trim: true,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+         match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email"]
+
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    verifyCode: {
+        type: String,
+        required: true,
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code Expiry is required"]
+
+    },
+    isVerified:{
+        type: Boolean,
+        default: false,
+    },
+
+    isAcceptingMessage:{
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema]
+
+
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
