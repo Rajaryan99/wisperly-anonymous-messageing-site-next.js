d2fb35344801fc6535acf4dd80663c2ed72fe724
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-09 14:16:38 +0530

    added DB connection

--- src/Schemas/messageSchema.ts
+++ src/Schemas/messageSchema.ts
@@ -1,4 +1,4 @@
-import { Content } from "next/font/google";
+import { start } from "repl";
 import { z } from "zod";
 
 export const messageSchema = z.object({
@@ -8,3 +8,5 @@ export const messageSchema = z.object({
         .max(300, {message: 'content must be not more then 300 character'})
 
 })
+
+// in express or node.js if once we connect the DB and start the DB connection the server keeps on running that why we use cloude instence because backend should run continusly, whereas Next.js is completely different framework, it a edge time framework, in this the resource dont run continusly all the time as soon as user hit the request  the things gets exicuted. and specially when we write functons and all they run on time, whereas in other backend services once its connected the things keeps on runnning.
