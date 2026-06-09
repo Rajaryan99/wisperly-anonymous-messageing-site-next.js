546eaa394e6e610ba727aaae6ef764100c24d1f1
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-09 12:53:17 +0530

    Added message schema with Zod message validation

--- src/Schemas/messageSchema.ts
+++ src/Schemas/messageSchema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const messageSchema = z.object({
     content: z
         .string()
-        .min(10, {message: 'content must be of atleast 10 character'})
+        .min(10, {message: 'content must be of atleast 10 character'}) 
         .max(300, {message: 'content must be not more then 300 character'})
 
 })
