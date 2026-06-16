d03964b82ea82073250889ac3c6f312ea56c8a69
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-16 21:28:09 +0530

    nextAuth setup credentials

---
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,54 @@
+import { NextAuthOptions } from "next-auth";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        
+        CredentialsProvider({
+            id:  "credentials",
+            name: "Credentials",
+            Credential: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+
+            async authorize(credentials: any): Promise<any>{
+
+                await dbConnect();
+
+                try {
+
+                 const user =    await UserModel.findOne({
+                        $or: [
+                            {email: credentials.identifier},
+                            {username: credentials.identifier}
+                        ]
+                    })
+
+                        if(!user){
+                            throw new Error("No user found with this credientials")
+                        }
+
+                        if(!user.isVerified){
+                            throw new Error("The user is not verified")
+                        }
+
+                        const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+
+                        if(isPasswordCorrect){
+                            return user
+                        } else {
+                            throw new Error("Incorrect password")
+                        }
+                    
+                } catch (error: any) {
+                    throw new Error(error)
+                }
+            }
+        })
+    ]
+}
